ed9f97f29023a28368a2bc179d71ab7a09a3e2a3
KEVIN ECHEVERRI <ikevin1992@gmail.com>
2022-09-15 20:38:30 -0500

    UPDATE

--- src/pages/SellRegisterPage/components/EightStep/EightStep.tsx
+++ src/pages/SellRegisterPage/components/EightStep/EightStep.tsx
@@ -1,4 +1,3 @@
-import compress from 'compress-base64';
 import { useEffect, useRef, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { useLocation, useNavigate } from 'react-router-dom';
